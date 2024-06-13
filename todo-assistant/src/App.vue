f86c51265797af2018f13e65a654805f75e4da99
pigBond <wfkmsx@outlook.com>
2024-06-13 10:26:19 +0800

    0.3

---
+++ todo-assistant/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <p>Welcome to your Electron application.</p>
+</template>
+
+<script setup>
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
